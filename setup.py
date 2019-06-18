461c40ce7d679fcacf2fc581aa18193ff87b9a1c
Micah Cooper <micahcooper@cedarville.edu>
2019-06-18 18:09:09 -0400

    Fixed typo

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=['tdxlib'],
     url='https://github.com/cedarville-university/tdxlib',
     license='GNU General Public License v3.0',
-    long_description='This module provides options for programmatically affectin TDX objects, including tickets, '
+    long_description='This module provides options for programmatically affecting TDX objects, including tickets, '
                      'people, accounts, assets, and groups.',
     install_requires=['python-dateutil','requests', 'xlrd'],
     python_requires='>=3.6'
