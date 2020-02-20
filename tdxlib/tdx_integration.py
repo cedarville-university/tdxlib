68cddbad0a8aa3a0374e6b57e05c676840c9e35c
Josiah Lansford <jrlansford@cedarville.edu>
2020-02-20 15:21:36 -0500

    small change

--- tdxlib/tdx_integration.py
+++ tdxlib/tdx_integration.py
@@ -34,7 +34,7 @@ class TDXIntegration:
         self.username = None
         self.password = None
         self.token = None
-        self.token_exp
+        self.token_exp = None
         self.config = configparser.ConfigParser()
 
         # Read in configuration
