2d2f7ac13151dfdd67202188a762f2f1ab63bc63
Nat Biggs <nbiggs112@cedarville.edu>
2021-03-04 11:33:29 -0500

    fixing long description

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
-with open("README.md", 'r', encoding='utf-8"') as fh:
-    outer_long_description = fh.read()
-
 setup(
     name='TDXLib',
     description='a python library for interacting with the TeamDynamix Web API',
@@ -12,7 +9,8 @@ setup(
     packages=['tdxlib'],
     url='https://github.com/cedarville-university/tdxlib',
     license='GNU General Public License v3.0',
-    long_description=outer_long_description,
+    long_description='This module provides options for programmatically affecting TDX objects, including tickets, '
+                     'people, accounts, assets, and groups. Documentation at https://tdxlib.readthedocs.io',
     install_requires=['python-dateutil','requests', 'PYjwt'],
     python_requires='>=3.6'
 )
